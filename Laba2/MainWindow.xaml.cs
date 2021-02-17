a8b31ab577811af4d31f9cb69cea8c9cf3e3d853
Maksim1617 <valet1617@gmail.com>
2021-02-17 16:37:24 +0200

    Добавьте файлы проекта.

---
+++ Laba2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laba2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtName.Text.Length == 0)
+            {
+                MessageBox.Show("You haven't typed in a name!", "Error");
+                txtName.Focus();
+                return;
+            }
+            if (txtGroup.Text.Length == 0)
+            {
+                MessageBox.Show("You haven't typed in a Group!", "Error");
+                txtName.Focus();
+                return;
+            }
+            // otherwise, display name
+            MessageBox.Show("Welcome, " + txtName.Text + "student of group " + txtGroup.Text);
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
